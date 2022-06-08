a2eeca96f60795f7ea68b80bd46249545cc78b99
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-06-08 21:26:44 +0000

    make catkin_ws

---
+++ catkin_ws/build/slam_gmapping/slam_gmapping/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/workspace/navegation-warehouse/catkin_ws/src/slam_gmapping/slam_gmapping
+# Build directory: /home/user/workspace/navegation-warehouse/catkin_ws/build/slam_gmapping/slam_gmapping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
