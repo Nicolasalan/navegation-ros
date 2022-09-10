40f934ef4e5ec6daeb38aabfd97c9a9f8d07d50d
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-09-10 12:50:21 -0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -4,7 +4,5 @@ sudo apt-get install ros-$ROS_DISTRO-navigation
 sudo apt-get install ros-$ROS_DISTRO-joy
 sudo apt-get install ros-$ROS_DISTRO-teleop-twist-keyboard
 sudo apt-get install ros-$ROS_DISTRO-amcl
-sudo apt-get install ros-$ROS_DISTRO-neo-local-planner
 sudo apt-get install ros-$ROS_DISTRO-map-server
 sudo apt-get install ros-$ROS_DISTRO-move-base
-pip install simple-http-server
