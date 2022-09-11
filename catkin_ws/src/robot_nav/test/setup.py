01495362dc42074ff387af6b4aeaf73b92e40281
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-09-11 08:57:09 -0300

    Update setup.py

--- catkin_ws/src/robot_nav/test/setup.py
+++ catkin_ws/src/robot_nav/test/setup.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
-
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
+d = generate_distutils_setup(
     packages=['robot_nav'],
-    package_dir={'': 'test'},
+    package_dir={'': 'test'}
 )
 
-setup(**setup_args)
+setup(**d)
