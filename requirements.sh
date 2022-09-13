3dac749fa77bcd25e690565b0826893ab88bc407
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-09-13 14:46:51 -0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -6,5 +6,7 @@ sudo apt-get install ros-$ROS_DISTRO-teleop-twist-keyboard
 sudo apt-get install ros-$ROS_DISTRO-amcl
 sudo apt-get install ros-$ROS_DISTRO-map-server
 sudo apt-get install ros-$ROS_DISTRO-move-base
+sudo apt-get install ros-$ROS_DISTRO-rosbridge-suite
 
+sudo apt-get install npm
 chmod +x ~/catkin_ws/src/robot_nav/src/*
